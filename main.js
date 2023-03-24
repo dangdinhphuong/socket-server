d9c688facea77bac768bb04c6f190e0727adc551
Phuongdd <dangxuong2000@gmail.com>
2023-03-24 16:11:43 +0700

    update

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ const broadcast = (ws, message, includeSelf) => {
   keepAliveId = setInterval(() => {
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send('ping');
+        client.send({'status':'ping'});
       }
     });
   }, 50000);
